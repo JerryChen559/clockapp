0cd6e41e1bdfb38eaa8dd3ba64e00bc59cd55a2c
Jerry Chen <zgchen559@gmail.com>
2019-01-07 20:31:10 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var app = angular.module("clockApp", []);
+
+app.controller("TimeCtrl", TimeCtrl);
+
+function TimeCtrl($scope) {
+  var currentDate = new Date();
+  $scope.timeString = currentDate.toTimeString();
+  $scope.updateTime = function() {
+    console.log("Update Time button HIT");
+    var currentDate = new Date();
+    $scope.timeString = currentDate.toTimeString();
+  };
+  $scope.userName = "";
+}
